bbd33c8dcb1863ee12d50848a5f4b690f77572dd
Darja Sedova <dari.sedova@gmail.com>
2024-02-21 01:15:02 +0200

    chore: add logging

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from scraper import Scraper
+import logging
 
 
 def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [INFO] - %(message)s",
+    )
+
     scraper = Scraper(
         base_url="https://www.ss.com/lv/real-estate/flats/riga/centre/hand_over/",
         flat_url="https://www.ss.com/msg/lv/real-estate/flats/riga/centre/",
     )
 
-    for page_number in range(1, 10):
+    for page_number in range(1, 11):
+        logging.info(f"Scraping page {page_number}")
         scraper.scrape_page(page_number)
 
 
--- scraper.py
+++ scraper.py
@@ -1,17 +1,20 @@
+import logging
+
 import requests
 from bs4 import BeautifulSoup
 from tinydb import Query, TinyDB
 
 
 class Flat:
-    def __init__(self, id, url, description, price):
+    def __init__(self, id, index, url, description, price):
         self.id = id
+        self.index = index
         self.url = url
         self.description = description
         self.price = price
 
     def __str__(self):
-        return f"ID: {self.id}, URL: {self.url}, Description: {self.description}, Price: {self.price}"
+        return f"Nr: {self.index}, URL: {self.url}, Description: {self.description}, Price: {self.price}\n"
 
 
 class Scraper:
@@ -28,10 +31,13 @@ class Scraper:
         table = parsed_html.body.find("table", {"align": "center"})
         lines = table.find_all("tr")
 
+        index_counter = 0
+
         for line in lines[1:-1]:
-            flat_id = line.find_all("td")[1].find("a").get("href").split("/")[-1]
+            flat_id = line.find_all("td")[1].find("a").get("href").split("/")[-1].split(".")[0]
 
             if self.db.search(Query().id == flat_id):
+                logging.info(f"Flat {flat_id} already in the database")
                 continue
 
             price = line.find_all("td")[-1].text.split(" ")
@@ -40,11 +46,19 @@ class Scraper:
             if "mēn" not in price[2] or float(price[0].replace(",", "")) > 350:
                 continue
 
+            index_counter += 1
+
             flat_url = f"{self.flat_url}{flat_id}"
 
             flat = Flat(
-                id=flat_id, url=flat_url, description=description, price="".join(price)
+                id=flat_id,
+                index=index_counter,
+                url=flat_url,
+                description=description,
+                price="".join(price),
             )
+
+            logging.info(flat)
 
             self.db.insert(
                 {
@@ -55,10 +69,10 @@ class Scraper:
                 }
             )
 
-            self.notify(flat_url, flat.price)
+            self.notify(index_counter, flat_url, flat.price)
 
-    def notify(self, flat_url, price):
+    def notify(self, index, flat_url, price):
         requests.post(
             "http://ntfy.sh/flat-advertisments",
-            json={"url": flat_url, "price": price[0:3]},
+            json={"index": index, "url": flat_url, "price": price[0:3]},
         )
