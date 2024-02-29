63b14bc457ceb70d372ab9d94ce93c302c1cf75a
Darja Sedova <dari.sedova@gmail.com>
2024-02-29 19:51:48 +0200

    feat: add hometown support

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from scraper import Scraper
 import logging
 
+class url:
+    def __init__(self, hometown):
+        self.base_url = f"https://www.ss.com/lv/real-estate/flats/riga/{hometown}/hand_over/"
+        self.flat_url = f"https://www.ss.com/msg/lv/real-estate/flats/riga/{hometown}/"
+
+
+
 
 def main():
     logging.basicConfig(
@@ -13,9 +20,13 @@ def main():
         flat_url="https://m.ss.com/msg/lv/real-estate/flats/riga/centre/",
     )
 
-    for page_number in range(1, 11):
-        logging.info(f"Scraping page {page_number}")
-        scraper.scrape_page(page_number)
+    for hometown in hometowns:
+        logging.info(f"Scraping {hometown}")
+        scraper = Scraper(url(hometown).base_url, url(hometown).flat_url)
+
+        for page_number in range(1, 11):
+            logging.info(f"Scraping page {page_number}")
+            scraper.scrape_page(page_number)
 
 
 if __name__ == "__main__":
