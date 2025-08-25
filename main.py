f519ee6d333b2c4382f35873f64cdb50add6a93d
Darja Sedova <dari.sedova@gmail.com>
2025-08-25 22:59:13 +0300

    partially working version

--- main.py
+++ main.py
@@ -3,27 +3,13 @@ import logging
 from scraper import Scraper
 
 
-class url:
-    def __init__(self, hometown):
-        self.base_url = f"https://www.ss.com/lv/real-estate/flats/riga/{hometown}/hand_over/"
-        self.flat_url = f"https://www.ss.com/msg/lv/real-estate/flats/riga/{hometown}/"
-
-
 def main():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [INFO] - %(message)s",
     )
 
-    hometowns = ["centre", "grizinkalns"]
-
-    for hometown in hometowns:
-        logging.info(f"Scraping {hometown}")
-        scraper = Scraper(url(hometown).base_url, url(hometown).flat_url)
-
-        for page_number in range(1, 2):
-            logging.info(f"Scraping page {page_number}")
-            scraper.scrape_page(page_number)
+    scraper = Scraper()
 
 
 if __name__ == "__main__":
--- scraper.py
+++ scraper.py
@@ -1,90 +1,125 @@
+# pyright: basic
 import logging
-import os
 import time
-
 import requests
-from bs4 import BeautifulSoup
-from tinydb import Query, TinyDB
+import re
+import json
+from playwright.sync_api import sync_playwright
+import html
+import json
+from shapely.geometry import shape, Point
 
-DB_PATH = os.getenv("PS_DB_PATH", "./db.json")
-NTFY_URL = "https://ntfy.sh/tesy"
 
 
-class Flat:
-    def __init__(self, table: list, id: str, url: str) -> None:
+# from requests.models import default_hooks
+# from tinydb import Query, TinyDB
+
+
+class Appartment:
+    def __init__(self, id: int, index: int, url: str, coordinates: tuple, price: str):
         self.id = id
+        self.index = index
+        self.coordinates = coordinates
         self.url = url
-        self.street = table[3].text
-        self.rooms = table[4].text
-        self.area = table[5].text
-        self.floor = table[6].text
-        self.type = table[7].text
-        self.price = table[9].text.split(" ")
+        self.price = price
 
-    def __str__(self) -> str:
-        return f"Flat {self.id}: {self.street}, {self.rooms}, {self.area}, {self.floor}, {self.type}, {self.price}"
+    def __str__(self):
+        return f"Nr: {self.index}, URL: {self.url}, Price: {self.price}\n"
 
 
 class Scraper:
-    def __init__(self, base_url, flat_url) -> None:
-        self.base_url = base_url
-        self.flat_url = flat_url
-        self.db = TinyDB(DB_PATH)
+    def __init__(self):
+        self.url = "https://www.ss.com/lv/fTgTeF4QAzt4FD4eFFM=.html?map=17020&map2=17020&cat=14195&mode=3"
+        self.cookies_url = "https://www.ss.com/lv/real-estate/flats/riga/all/fDgQeF4S.html"
+        with open("bad_regions.geojson") as f:
+            geojson = json.load(f)
 
-    def scrape_page(self, page_number) -> None:
-        url = f"{self.base_url}page{page_number}.html"
-        response = requests.get(url)
+        self.bad_coordinates = [shape(feature["geometry"]) for feature in geojson["features"]]
+        # self.db = TinyDB("db.json")
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto(self.cookies_url)
+            self.cookies = context.cookies()
+            print("COOKIES: ", self.cookies[0]['value'])
+            self.scrape_page()
+            page.close()
 
-        parsed_html = BeautifulSoup(response.text, "html.parser")
-        if not parsed_html.body:
-            logging.error(f"Failed to parse page {page_number}")
-            return
+    def scrape_page(self):
+        cookies = {
+            "PHPSESSID": self.cookies[0]['value']
+        }
+        response = requests.get(self.url, cookies=cookies)
+        
+        match = re.search(r"var\s+MARKER_DATA\s*=\s*(\[.*?\]);", response.text, re.S)
+        marker_data = []
+        if match:
+            marker_data_str = match.group(1)  # the raw JS array
+            try:
+                marker_data = json.loads(marker_data_str)  # parse as JSON
+            except json.JSONDecodeError:
+                marker_data = marker_data_str
 
-        table = parsed_html.body.find("table", {"align": "center"})
-        if not table:
-            logging.error(f"Failed to find table in page {page_number}")
-            return
+        for data in marker_data:
+            elements = data.split("<br>")
+            elements = [html.unescape(item).replace("<b>", "").replace("</b>", "") for item in elements]
 
-        lines = table.find_all("tr")  # pyright: ignore
-        if not lines:
-            logging.error(f"Failed to find table in page {page_number}")
-            return
-
-        for line in lines[1:-1]:
-            flat_id = line.find_all("td")[1].find("a").get("href").split("/")[-1].split(".")[0]
-
-            # Check if the flat is already in the database
-            if self.db.search(Query().id == flat_id):
-                logging.info(f"Flat {flat_id} already in the database")
+            if len(elements) != 8:
                 continue
 
-            flat = Flat(line.find_all("td"), flat_id, f"{self.flat_url}{flat_id}.html")
+            if validCost(elements):
+                coordinates = tuple(elements[0].split("|")[0:2])
+                address = elements[1]
+                rooms = elements[2].split(" ")[1]
+                floor = elements[4].split(" ")[1].split("/")
+                # print(elements)
+                print(self.bad_coordinates[0])
+                # if validFloor(floor):
+                #     cost = elements[6]
+                #     if validNeighbourhood:
+                #         # print(coordinates, address, rooms, floor, cost)
 
-            # Check if the flat is rented by days or is too expensive
-            price = self.normalize_price(flat.price[0])
-            if "mēn" not in flat.price[2] or price > 330 or price < 200:
-                logging.info(f"Flat {flat_id} is too expensive")
-                continue
 
-            flat.price = flat.price[0]
 
-            logging.info(flat)
+            # self.db.insert(
+            #     {
+            #         "id": flat.id,
+            #         "url": flat.url,
+            #         "price": flat.price,
+            #     }
+            # )
 
-            self.db.insert({"id": flat.id, "url": flat.url})
+            # self.notify(flat_url, flat.price)
+            # time.sleep(0.5)  # to not hit ntfy rate limit
 
-            self.notify(flat)
-            time.sleep(0.5)  # to not hit ntfy rate limit
-            break
-
-    @staticmethod
-    def notify(flat: Flat):
-        requests.post(
-            "https://ntfy.sh/flat-advertisments",
-            headers={"Click": f"{flat.url}", "Title": "New flat found"},
-            data=f"{flat.street}, {flat.rooms} rooms, {flat.area} m2, {flat.floor} floor, {flat.type}, {flat.price} euro",
+    def notify(self, flat_url, price):
+        response = requests.post(
+            "https://ntfy.sh/tesy",
+            headers={
+                "Click": f"{flat_url}",
+                "Title": f"New flat found: {price[:3]}",
+            },
         )
 
-        logging.info("Notification sent")
+        logging.info(f"Notification sent. Code: {response.status_code}")
 
-    def normalize_price(self, price: str) -> float:
-        return float(price.replace(",", ""))
+def validCost(elements: list):
+    return "mēn" not in elements[6] and "dienā" not in elements[6] and "īret" not in elements[6] and not "pērku" in elements[5] and "vēlos" not in elements[5]
+
+def validFloor(floor: list) -> bool:
+    if len(floor) > 1:
+        return not floor[0] == floor[1] or int(floor[0]) >= 3
+    return int(floor[0]) >= 3
+
+def validRooms(rooms: int) -> bool:
+    return rooms <=3 and rooms >= 2
+
+def validNeighbourhood(coordinates: tuple) -> bool:
+    point = Point(coordinates)
+    print(self.bad_coordinates)
+    return any(poly.contains(point) for poly in self.bad_coordinates)
+
+        
+# 693828259240c49e4b7e704067aa99b0
+# 1fbe7847ec4682d57f81a8a6b9b5d48f
