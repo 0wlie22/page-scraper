fced236a61dbb875eb685339004e8f8f7b558c8a
TAASEDAE <dari.sedova@gmail.com>
2025-08-26 12:11:52 +0300

    chore: working version

--- scraper.py
+++ scraper.py
@@ -1,15 +1,13 @@
-# pyright: basic
-import logging
-import time
-import requests
-import re
-import json
-from playwright.sync_api import sync_playwright
+# pyright:basic
 import html
 import json
-from shapely.geometry import shape, Point
+import logging
+import re
 
-
+# import time
+import requests
+from playwright.sync_api import sync_playwright
+from shapely.geometry import Point, shape
 
 # from requests.models import default_hooks
 # from tinydb import Query, TinyDB
@@ -30,11 +28,15 @@ class Appartment:
 class Scraper:
     def __init__(self):
         self.url = "https://www.ss.com/lv/fTgTeF4QAzt4FD4eFFM=.html?map=17020&map2=17020&cat=14195&mode=3"
-        self.cookies_url = "https://www.ss.com/lv/real-estate/flats/riga/all/fDgQeF4S.html"
+        self.cookies_url = (
+            "https://www.ss.com/lv/real-estate/flats/riga/all/fDgQeF4S.html"
+        )
         with open("bad_regions.geojson") as f:
             geojson = json.load(f)
 
-        self.bad_coordinates = [shape(feature["geometry"]) for feature in geojson["features"]]
+        self.bad_coordinates = [
+            shape(feature["geometry"]) for feature in geojson["features"]
+        ]
         # self.db = TinyDB("db.json")
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
@@ -42,16 +44,14 @@ class Scraper:
             page = context.new_page()
             page.goto(self.cookies_url)
             self.cookies = context.cookies()
-            print("COOKIES: ", self.cookies[0]['value'])
+            print("COOKIES: ", self.cookies[0]["value"])
             self.scrape_page()
             page.close()
 
     def scrape_page(self):
-        cookies = {
-            "PHPSESSID": self.cookies[0]['value']
-        }
+        cookies = {"PHPSESSID": self.cookies[0]["value"]}
         response = requests.get(self.url, cookies=cookies)
-        
+
         match = re.search(r"var\s+MARKER_DATA\s*=\s*(\[.*?\]);", response.text, re.S)
         marker_data = []
         if match:
@@ -63,24 +63,23 @@ class Scraper:
 
         for data in marker_data:
             elements = data.split("<br>")
-            elements = [html.unescape(item).replace("<b>", "").replace("</b>", "") for item in elements]
+            elements = [
+                html.unescape(item).replace("<b>", "").replace("</b>", "")
+                for item in elements
+            ]
 
             if len(elements) != 8:
                 continue
 
-            if validCost(elements):
+            if validCost(elements[6]):
                 coordinates = tuple(elements[0].split("|")[0:2])
+                neighbourhood = elements[0].split("|")[2].split(" ")[1]
                 address = elements[1]
                 rooms = elements[2].split(" ")[1]
                 floor = elements[4].split(" ")[1].split("/")
-                # print(elements)
-                print(self.bad_coordinates[0])
-                # if validFloor(floor):
-                #     cost = elements[6]
-                #     if validNeighbourhood:
-                #         # print(coordinates, address, rooms, floor, cost)
-
-
+                cost = elements[6].split(" ")[1]
+                if self.validNeighbourhood(coordinates):
+                    print(" | ".join([neighbourhood, address, rooms, floor[0], cost]))
 
             # self.db.insert(
             #     {
@@ -104,22 +103,37 @@ class Scraper:
 
         logging.info(f"Notification sent. Code: {response.status_code}")
 
-def validCost(elements: list):
-    return "mēn" not in elements[6] and "dienā" not in elements[6] and "īret" not in elements[6] and not "pērku" in elements[5] and "vēlos" not in elements[5]
+
+    def validNeighbourhood(self, coordinates: tuple) -> bool:
+        coordinates = (float(coordinates[0]), float(coordinates[1]))
+        point = Point(coordinates[1], coordinates[0])
+        for polygon in self.bad_coordinates:
+            # print("CHECKING: ", coordinates, polygon)
+            if polygon.contains(point):
+                return False
+        return True
+
+def validCost(cost: str) -> bool:
+    try:
+        price = int(cost.split(" ")[1].replace(",", ""))
+        if price <= 100000 and price >= 30000:
+            return True
+    except ValueError:
+        return False
+    return False
+
 
 def validFloor(floor: list) -> bool:
     if len(floor) > 1:
         return not floor[0] == floor[1] or int(floor[0]) >= 3
     return int(floor[0]) >= 3
 
+
 def validRooms(rooms: int) -> bool:
-    return rooms <=3 and rooms >= 2
+    return rooms <= 3 and rooms >= 2
 
-def validNeighbourhood(coordinates: tuple) -> bool:
-    point = Point(coordinates)
-    print(self.bad_coordinates)
-    return any(poly.contains(point) for poly in self.bad_coordinates)
 
-        
+
+
 # 693828259240c49e4b7e704067aa99b0
 # 1fbe7847ec4682d57f81a8a6b9b5d48f
